9834eb6d8993d5cbcc90aac1175c97f1624e7406
Lucas Edson <lucasedson654@gmail.com>
2024-10-12 16:12:41 -0300

    Aula 0 OK

---
+++ Aula 0/a0.py
@@ -0,0 +1,18 @@
+import math
+
+# def celsiusToFah(c):
+#     c = float(c)
+#     return (9/5*c) + 32
+
+# def FahToCel():
+#     ...
+
+# while True:
+#     c = input("Digite o valor em Celsius: ")
+#     if c == "sair":
+#         break
+
+#     print(celsiusToFah(c))
+
+
+print(math.sin(math.radians(90)))
